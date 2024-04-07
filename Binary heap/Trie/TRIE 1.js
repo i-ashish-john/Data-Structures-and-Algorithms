40adcbfb5c6527aeb3952bc736b2c0dc7c9a250a
Ashish john <johnashish509@gmail.com>
2024-04-07 23:59:29 +0530

    updated

--- Binary heap/Trie/TRIE 1.js
+++ Binary heap/Trie/TRIE 1.js
@@ -1,1 +1,25 @@
- 
+ class Node{
+    constructor(){
+        this.children={}
+        this.WordEnd=false
+    }
+ }
+
+ class tire{
+    constructor(){
+        this.root=new Node()
+    }
+
+    insert(word){
+        for(let i=0;i<word.length;i++){
+            const char = word[i]
+            if(!this.children[char]){
+                this.root=new Node()
+            }
+            this.root=this.children[char]
+        }
+    }
+
+ }
+
+ const trieNode= new trie()
