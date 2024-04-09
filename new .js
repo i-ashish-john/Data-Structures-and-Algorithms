dd9f38f725bcce376b79adf9b1b17ed96b07f5a0
Ashish john <johnashish509@gmail.com>
2024-04-10 00:48:09 +0530

    updated

--- new .js
+++ new .js
@@ -1,48 +1,55 @@
-insert(){
-    const node=  new Node()
-    if(this.isEmpty()){
-        return null
-    }
-    else{
-        this.insertNode(this.root,target)
-    }
-}
-insertNode(root,target){
-if(!root){
-    this.root=target.value
-}
-if(root.value<target.value){
-    root.left=target.value
-    if(root.left===null){
-        root.left=target
-    }
+// insert(){
+//     const node=  new Node()
+//     if(this.isEmpty()){
+//         return null
+//     }
+//     else{
+//         this.insertNode(this.root,target)
+//     }
+// }
+// insertNode(root,target){
+// if(!root){
+//     this.root=target.value
+// }
+// if(root.value<target.value){
+//     root.left=target.value
+//     if(root.left===null){
+//         root.left=target
+//     }
 
-}else{
-    root.right=target.value
-    if(root.right===null){
-        root.right=target
-    }
+// }else{
+//     root.right=target.value
+//     if(root.right===null){
+//         root.right=target
+//     }
 
-}
+// }
 
-}
+// }
 
-search(target){
-  if(this.isEmpty()){
-    return null
-  }
-this.searchNode(target)
-}
+// search(target){
+//   if(this.isEmpty()){
+//     return null
+//   }
+// this.searchNode(target)
+// }
 
-searchNode(){
+// searchNode(){
 
-}
+// }
 
 
 
- preOrder(){
-    if(this.isEmpty()){
-        return false
-    }
-    const leftchildindex=
- }
+//  preOrder(){
+//     if(this.isEmpty()){
+//         return false
+//     }
+//     const leftchildindex=
+//  }
+
+
+// for(let i=1;i<=5;i++){
+//     for(let j=0;j<i;j++){
+//         console.log("*")
+//     }
+// }
