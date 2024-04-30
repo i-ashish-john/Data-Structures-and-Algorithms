b02d37c5d0e5a69849d59c5bf91ec4a026f9e7d9
Ashish john <johnashish509@gmail.com>
2024-04-30 21:47:38 +0530

    updated

--- Array/Array2D.js
+++ Array/Array2D.js
@@ -7,9 +7,11 @@
 const array2D=[[0,1,1],[0,0,0],[1,1,0]]
 
 let count = 0;
+
 let value=[]
 
 for(let i=0;i<array2D.length;i++){
+
 
     const value = array2D[i]
     for(let j=0;j<value.length;j++){
